844ec8ed7457e0415a410ec757f0c0db85961975
Mason Balbera <mhbalbera@gmail.com>
2019-10-02 10:58:47 -0400

    complete top scores API functionality

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     
-    def my_top_5
+    def my_top
         user = User.find_by(name: params[:name])
         scores = user.scores.sort_by(|score| score)
-        render json: scores[0...5]
+        render json: scores
     end
 
 end
--- Backend/app/models/score.rb
+++ Backend/app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ApplicationRecord
 
-    belongs_to: user
+    belongs_to :user
     
 end
